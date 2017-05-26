bde52bffd50ce32de181453eb2dfaf8339dc5474
vrk <vrkwork@gmail.com>
2017-05-25 22:08:49 -0700

    fix

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser');
 const googleSheets = require('gsa-sheets');
 
 // TODO(you): Update the contents of privateSettings accordingly, as you did
-// in HW5.
-const key = require('./privateSettings.json');
+// in HW5, then uncomment this line.
+// const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
-// GSA spreadsheet, as you did in HW5.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+// GSA spreadsheet, as you did in HW5, then uncomment these lines.
+// const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+// const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
 const app = express();
 const jsonParser = bodyParser.json();
-const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
 app.use(express.static('public'));
 

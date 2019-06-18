5a91653095a50b32614a737ec4c9732cf74cb3ec
Rax Cruz <raxcruz@Raxde-MacBook-Pro.local>
2019-06-18 12:03:26 +0800

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
-//
-// TODO(you): Add the JavaScript necessary to complete your final project.
-//
+function form_receive(){
+    const usernameElement = document.getElementById('username');
+    const passwordElement = document.getElementById('password');
+    const username = usernameElement.value;
+    const password = passwordElement.value;
+    console.log(username+password);
+}
+function register(){
+    const usernameElement = document.getElementById('username');
+    const passwordElement = document.getElementById('password');
+    const username = usernameElement.value;
+    const password = passwordElement.value;
+    console.log(username+password);
+}
+function sign_up(){
+    const boxElement = document.querySelector('.box');
+    const boxfrontElement = document.querySelector('.box .front');
+    const boxbackElement = document.querySelector('.box .back');
+    boxbackElement.style.transform = 'rotateY(180deg)';
+    boxElement.style.transform = 'rotateY(-180deg)';
+    boxfrontElement.classList.add('hidden');
+    boxbackElement.classList.remove('hidden');
+}
+function login(){
+    const boxElement = document.querySelector('.box');
+    const boxfrontElement = document.querySelector('.box .front');
+    const boxbackElement = document.querySelector('.box .back');
+    boxbackElement.style.transform = 'rotateY(180deg)';
+    boxElement.style.transform = 'rotateY(0deg)';
+    boxfrontElement.classList.remove('hidden');
+    boxbackElement.classList.add('hidden');
+}
